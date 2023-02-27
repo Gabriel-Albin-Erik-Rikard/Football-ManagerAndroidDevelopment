dc1552972574a974c654ae695c5df205aeb2dd06
Gabriel Adward <adga21bv@student.ju.se>
2023-02-27 09:25:26 +0100

    ViewAll screen for news work

--- app/src/main/java/com/example/football_manager/MainActivity.kt
+++ app/src/main/java/com/example/football_manager/MainActivity.kt
@@ -3,7 +3,17 @@ package com.example.football_manager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,7 +30,60 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val now = LocalDateTime.now()
+val newsRepository = NewsRepository().apply{
+    addNews(
+        "Welcome to Ekhagen",
+        "In ekhagen you can find........",
+        date = now.toString(),
+        "Gabriel Adward"
+    )
+}
 
+
+
+class NewsRepository {
+    private val multipleNews = mutableListOf<News>()
+
+    fun addNews(title: String, content: String, date: String, writer: String): Int {
+        val id = when {
+            multipleNews.isEmpty() -> 1
+            else -> multipleNews.last().id + 1
+        }
+
+        multipleNews.add(
+            News(
+                id,
+                title,
+                content,
+                date,
+                writer
+            )
+        )
+        return id
+    }
+
+    fun getAllNews() = multipleNews
+
+    fun getNewsById(id:Int) = multipleNews.remove(multipleNews.find {
+        it.id == id
+    })
+
+    fun deleteNewsById(id:Int) = multipleNews.remove(multipleNews.find {
+        println(it.id)
+        it.id == id
+
+    })
+
+    fun updateNewsById(id: Int, newTitle: String, newContent:String, newDate:String){
+        getNewsById(id)?.run {
+            //title = newTitle
+            //content = newContent
+            //date = newDate
+        }
+    }
+
+}
 
 @Composable
 fun AppScreen() {
@@ -31,7 +94,7 @@ fun AppScreen() {
         startDestination = "viewAll"
     ){
         composable("viewAll") {
-            ViewAllScreen(navController, NewsRepository().getAllNews())
+            ViewAllScreen(navController, newsRepository.getAllNews())
         }
     }
 }
@@ -39,7 +102,17 @@ fun AppScreen() {
 @Composable
 fun ViewAllScreen(navController: NavHostController, listy: List<News>) {
 
+    LazyColumn(
+        modifier = Modifier.padding(24.dp)
+    ){
+        items(listy) {News ->
+            Column(modifier = Modifier.clickable { navController.navigate("viewwOne/${News.id}") }) {
+                Text(News.title, style = MaterialTheme.typography.titleLarge)
+                println(News.title)
+                Divider()
+            }
+        }
+    }
+
 }
-
-
 
