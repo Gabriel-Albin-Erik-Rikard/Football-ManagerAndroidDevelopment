cd1873835d3d068dca4b314025f917412ad8473b
Gabriel Adward <adga21bv@student.ju.se>
2023-02-28 21:06:29 +0100

    Added class ActivityRepository to manipulate data

--- app/src/main/java/com/example/football_manager/CalenderScreen.kt
+++ app/src/main/java/com/example/football_manager/CalenderScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 
 
 data class Activities(
@@ -18,13 +21,72 @@ data class Activities(
     var date: String,
     var time: String
 )
+
+class ActivityRepository {
+    private val multipleActivities = mutableListOf<Activities>()
+
+    fun addActivity(
+        matchType: String,
+        title: String,
+        description: String,
+        week: Int,
+        date: String,
+        time: String
+    ): Int {
+        val id = when {
+            multipleActivities.isEmpty() -> 1
+            else -> multipleActivities.last().id + 1
+        }
+        multipleActivities.add(
+            Activities(
+                id,
+                matchType,
+                title,
+                description,
+                week,
+                date,
+                time
+            )
+        )
+        return id
+    }
+
+    fun getAllActivities() = multipleActivities
+
+    fun getActivityById(id: Int) = multipleActivities.find {
+        it.id == id
+    }
+
+    fun deleteActivityById(id: Int): Boolean {
+        val activityToRemove = multipleActivities.find { it.id == id }
+        if (activityToRemove != null) {
+            return multipleActivities.remove(activityToRemove)
+        }
+        return false
+    }
+
+    fun updateActivityById(
+        id: Int,
+        newMatchType: String,
+        newTitle: String,
+        newDescription: String,
+        newWeek: Int,
+        newDate: String,
+        newTime: String
+    ) {
+        getActivityById(id)?.run {
+            matchType = newMatchType
+            title = newTitle
+            description = newDescription
+            week = newWeek
+            date = newDate
+            time = newTime
+        }
+    }
+}
+
+
 @Composable
 fun CalenderScreen(){
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green),
-    ){
-        Text(text = "CALENDER")
-    }
+
 }
