394d8c2740540be0b74b3a9899de1c096442a869
Gabriel Adward <adga21bv@student.ju.se>
2023-02-28 15:48:52 +0100

    Added content to route: EditNews

--- app/src/main/java/com/example/football_manager/HomeScreen.kt
+++ app/src/main/java/com/example/football_manager/HomeScreen.kt
@@ -356,6 +356,84 @@ fun CreateNews(navController: NavHostController) {
 }
 
 
+@ExperimentalMaterial3Api
+@Composable
+fun EditNews(id: Int, navController: NavHostController){
+
+    val errors = remember { mutableStateListOf<String>() }
+    val EDIT_NEWS_TITLE_MIN_LENGTH = 3
+    val EDIT_NEWS_TITLE_MAX_LENGTH = 30
+    val EDIT_NEWS_CONTENT_MIN_LENGTH = 10
+    val EDIT_NEWS_CONTENT_MAX_LENGTH = 120
+
+    val CurrentTaskID = newsRepository.getNewsById(id)
+    var CurrentTitle = CurrentTaskID?.title
+    var CurrentDescription = CurrentTaskID?.content
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            var InputTitleTextOfNews by remember { mutableStateOf(TextFieldValue(CurrentTitle.toString())) }
+            OutlinedTextField(
+                value = InputTitleTextOfNews,
+                onValueChange = {
+                    InputTitleTextOfNews = it
+                },
+                label = { androidx.compose.material3.Text("Your Title") },
+                placeholder = { androidx.compose.material3.Text("Write your title") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            var InputContentOfNews by remember { mutableStateOf(TextFieldValue(CurrentDescription.toString())) }
+            OutlinedTextField(
+                value = InputContentOfNews,
+                onValueChange = {
+                    InputContentOfNews = it
+                },
+                label = { androidx.compose.material3.Text("Your Content") },
+                placeholder = { androidx.compose.material3.Text("Write Your Content") }
+            )
+
+
+            var updateTitleField = InputTitleTextOfNews.text
+            var updateContentField = InputContentOfNews.text
+            Spacer(modifier = Modifier.height(16.dp))
+
+            androidx.compose.material3.Button(
+                onClick = {
+                    errors.clear()
+                    if (updateTitleField.isEmpty() or updateContentField.isEmpty()) {
+                        errors.add("Check So No Fields Are Empty")
+                        println("Hello")
+                    } else if ((updateTitleField.length < EDIT_NEWS_TITLE_MIN_LENGTH || updateTitleField.length > EDIT_NEWS_TITLE_MAX_LENGTH)) {
+                        errors.add("The Title Should Be Between 3-30 Characters")
+                    } else if (updateContentField.length < EDIT_NEWS_CONTENT_MIN_LENGTH || updateContentField.length > EDIT_NEWS_CONTENT_MAX_LENGTH) {
+                        errors.add("The Content Should Be Between 10-120 Characters")
+                    } else {
+
+                        newsRepository.updateNewsById(
+                            id,
+                            newTitle = updateTitleField,
+                            newContent = updateContentField
+                        )
+                        navController.popBackStack()
+                    }
+
+                },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                androidx.compose.material3.Text("Submit")
+            }
+            for (error in errors) {
+                androidx.compose.material3.Text(error)
+            }
+        }
+    }
+}
 
 
 
