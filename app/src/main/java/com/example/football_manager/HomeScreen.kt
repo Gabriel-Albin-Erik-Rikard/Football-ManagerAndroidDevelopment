a5ba131aeeca8aaef3e19761f37b194765bb986a
Gabriel Adward <adga21bv@student.ju.se>
2023-02-28 15:29:52 +0100

    Fixed Data Class News

--- app/src/main/java/com/example/football_manager/HomeScreen.kt
+++ app/src/main/java/com/example/football_manager/HomeScreen.kt
@@ -33,8 +33,11 @@ import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 //FREE TO CHANGE
 
 data class News(
-    var title : String,
-    var content : String,
+    val id: Int,
+    var title: String,
+    var content: String,
+    var date: String,
+    var writer: String
 )
 
 val newsList = mutableListOf<News>()
