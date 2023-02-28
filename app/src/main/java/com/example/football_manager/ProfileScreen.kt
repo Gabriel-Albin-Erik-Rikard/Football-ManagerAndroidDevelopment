cb2736c7646185facb1413c0492663db445e4e11
Albin Forsberg <foal21fs@student.ju.se>
2023-02-28 09:57:45 +0100

    Added userProfile card

--- app/src/main/java/com/example/football_manager/ProfileScreen.kt
+++ app/src/main/java/com/example/football_manager/ProfileScreen.kt
@@ -1,20 +1,76 @@
 package com.example.football_manager
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun ProfileScreen(){
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green),
+            .height(350.dp)
+            .fillMaxWidth()
+            .padding(16.dp)
     ){
-        Text(text = "PROFILE")
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Profile picture
+                Box(
+                    modifier = Modifier
+                        .size(120.dp)
+                        .padding(4.dp)
+                        .clip(RoundedCornerShape(50.dp))
+                        .border(2.dp, androidx.compose.ui.graphics.Color.Black, RoundedCornerShape(50.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // TODO: Add profile picture
+                    Text(
+                        text = "Profile picture",
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                // Name
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Name",
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+            }
+        }
     }
 }
