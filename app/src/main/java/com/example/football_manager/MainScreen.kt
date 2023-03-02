0e3edf42a0713f72d01ed6ac882b6e256932fb14
Hellberg95 <heri21mm@student.ju.se>
2023-03-02 13:36:42 +0100

    firstOutcast

--- app/src/main/java/com/example/football_manager/MainScreen.kt
+++ app/src/main/java/com/example/football_manager/MainScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
@@ -15,6 +16,7 @@ import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.selects.select
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
--- app/src/main/java/com/example/football_manager/TeamsScreen.kt
+++ app/src/main/java/com/example/football_manager/TeamsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.football_manager
 
-import android.media.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,156 +7,214 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.data.Group
-import androidx.compose.ui.tooling.data.UiToolingDataApi
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-//TEST DATA
-data class Group(var id: Int,
-                 var name : String,
-                 val players: MutableList<Player>
-)
-data class Player(
-    var id : Int,
-    var assists : Int,
-    var yellowCards : Int,
-    var gamesPlayed : Int,
-    var name : String,
-    var goals : Int,
-    var redCards : Int
-)
+class Group(var id : Int, var name : String ){
+    val players = mutableListOf<Player>()
+    val listOfStaff = mutableListOf<Staff>()
+    fun addPlayer(name : String){
+        val id = when{
+            players.isEmpty() -> 0
+            else -> players.last().id + 1
+        }
+        players.add(Player(id, name))
+    }
+    fun addStaff(name: String){
+        val id = when{
+            listOfStaff.isEmpty() -> 0
+            else -> listOfStaff.last().id + 1
+        }
+        listOfStaff.add(Staff(id, name))
+    }
+    fun getPlayerById(id: Int): Player {
+        return players[id]
+    }
+}
 
-fun createTest() {
-    val group1 = Group(1, "Team A", mutableListOf())
-    val player1 = Player(1, 2, 2, 2, "Jöken", 2, 3)
-    val player2 = Player(2, 3, 4, 4, "Boris", 4, 5)
-    group1.players.add(player1)
-    group1.players.add(player2)
+data class Staff( val id : Int, val name: String){
+    var role: String = ""
+}
+data class Player(val id : Int, val name : String){
+    var assists : Int = 0
+    var yellowCards : Int = 0
+    var gamesPlayed : Int = 0
+    var goals : Int = 0
+    var redCards : Int = 0
+}
 
-    val group2 = Group(1, "Team A", mutableListOf())
-    val player3 = Player(1, 2, 2, 2, "Dala", 2, 3)
-    val player4 = Player(2, 3, 4, 4, "Hästen", 4, 5)
-    group2.players.add(player3)
-    group2.players.add(player4)
+//test class, använder ej nu
+class PlayerGroupConnector {
+    private val playerToGroups = mutableMapOf<Int, MutableList<Int>>()
 
+    fun addPlayerToGroup(playerId: Int, groupId: Int) {
+        if (!playerToGroups.containsKey(playerId)) {
+            playerToGroups[playerId] = mutableListOf()
+        }
+        playerToGroups[playerId]?.add(groupId)
+    }
+
+    fun getGroupsForPlayer(playerId: Int): MutableList<com.example.football_manager.Group> {
+        val groupIds = playerToGroups[playerId]
+        val groups = mutableListOf<com.example.football_manager.Group>()
+        groupIds?.forEach {
+            groups.add(Group(it, "Group $it")) // You can replace this with your own logic for fetching the group data
+        }
+        return groups
+    }
+}
+
+val groupRepository = GroupRepository().apply {
+    addGroup("SexySwingers")
+    addGroup("FinaFynd")
+    addGroup("SexySwingers")
+    addGroup("FinaFynd")
+    addGroup("SexySwingers")
+    addGroup("FinaFynd")
+    addGroup("SexySwingers")
+    addGroup("FinaFynd")
+
+    getGroupById(0).addStaff("Herman")
+    getGroupById(0).addStaff("Börje")
+    getGroupById(0).addPlayer("Dala")
+    getGroupById(0).addPlayer("Jöken")
+    getGroupById(0).addPlayer("Kroken")
+
+    getGroupById(0).players[0].goals = 2
 }
 
 
+class GroupRepository(){
+    private val listOfGroups = mutableListOf<com.example.football_manager.Group>()
+
+    fun addGroup(name: String)  {
+        val id = when {
+            listOfGroups.isEmpty() -> 0
+            else -> listOfGroups.last().id +1
+        }
+        listOfGroups.add(
+            com.example.football_manager.Group(
+                id,
+                name
+            )
+        )
+    }
+    /*fun addGroup(group: com.example.football_manager.Group) {
+        val id = when {
+            listOfGroups.isEmpty() -> 0
+            else -> listOfGroups.last().id +1
+        }
+        listOfGroups.add(group)
+    }*/
+
+    fun getAllGroups() = listOfGroups
+    fun getGroupById(id: Int): com.example.football_manager.Group  {
+        return listOfGroups[id]
+    }
+}
+
 @Composable
 fun TeamsScreen(){
-    val groups = listOf(
-        Group(1, "Team A", mutableListOf()),
-        Group(2, "Team B", mutableListOf()),
-        Group(3, "Team C", mutableListOf())
-    )
-
     val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = "GroupScreenStart"
     ){
         composable("GroupScreenStart") {
-            GroupScreenStart(navController)
+            GroupScreenStart(navController,  groupRepository.getAllGroups())
         }
-        /*composable("GroupPage") {
-            GroupPage(navController)
+        composable("GroupPage/{id}") {
+            val id = it.arguments!!.getString("id")!!.toInt()
+            GroupPage(navController, id)
         }
-        composable("PlayerPage/{id}") {
-            val id = it.arguments?.getString("id")!!.toInt()
-            PlayerPage(id,navController)
-        }*/
+        composable("PlayerPage/{groupID}/{playerID}") {
+            println("kanske hit 0")
+            val groupID = it.arguments!!.getString("groupID")!!.toInt()
+            val playerID = it.arguments!!.getString("playerID")!!.toInt()
+            println("kanske hit 1")
+            PlayerPage(navController, groupID, playerID)
+        }
     }
 }
 
 @Composable
-fun GroupScreenStart(navController: NavController){
-
+fun GroupScreenStart(navController: NavController, groups : List<com.example.football_manager.Group>){
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = 50.dp)
-            .background(Color.Green),
-    ){
-        LazyColumn(
-        modifier = Modifier
-            .background(color = Color.Blue)
-            .fillMaxSize(1f),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-
-
-    )
-    {
-        item{
-            Button(onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
-            )   {
-                Text(text = "Create(implement later)")
-                }
-            }
-        //maybe use text here instead of buttons?
-        //Gillar Card
-            /*items(Groups){ group ->
-                Card(
-                    //shape =
-                    shape = RoundedCornerShape(12.dp),
-                    modifier = Modifier
-                        .height(80.dp)
-                        .width(280.dp)
-                        //.alignment = Alignment.Center
-                        //.background(androidx.compose.ui.graphics.Color.LightGray)
-                        .padding(12.dp)
-                        .clickable {
-                            //ta mig vidare här
-                            navController.navigate("GroupPage")
-                            println("hejhej")
-                        }
-                ) {
-                    Text(
-                        text = group.name,
-                        style = MaterialTheme.typography.h4,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            //.padding(12.dp)
-                            .background(Color.LightGray)
-                    )
-                }
-            }
-        }*/
-    }
-}
-
-@Composable
-fun GroupPage(navController: NavController){
-    Box(
-    modifier = Modifier
-        //.fillMaxSize()
-        .padding(bottom = 50.dp)
-        .background(Color.Blue),
-    ){
+            //.background(Color.Green),
+    ) {
         LazyColumn(
             modifier = Modifier
-                .background(color = Color.Blue)
-                .padding()
+                //.background(color = Color.Blue)
                 .fillMaxSize(1f),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(12.dp)
 
+        )
+        {
+            item {
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
+                ) {
+                    Text(text = "Create(implement later)")
+                }
+            }
+            items(groups) { group ->
+                Column(modifier = Modifier.padding(vertical = 15.dp)
+                    .clickable { navController.navigate("GroupPage/${group.id}") }) {
+                    Surface(
+                        color = MaterialTheme.colorScheme.secondary,
+                        shape = RoundedCornerShape(150.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                            .width(1100.dp)
+                    ) {
+                        Text(
+                            text = group.name,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(horizontal = 25.dp, vertical = 15.dp),
+                            color = androidx.compose.ui.graphics.Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun GroupPage(navController: NavController, id: Int){
+    val group = groupRepository.getGroupById(id)
+    val hej = group.id
+    println("grupp: ")
+    println(hej)
+    Box(
+    modifier = Modifier
+        //.fillMaxSize()
+        .padding(bottom = 50.dp)
+        //.background(Color.Blue),
+    ){
+        LazyColumn(
+            modifier = Modifier
+                //.background(color = Color.Blue)
+                .padding()
+                .fillMaxSize(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(2.dp)
         )
         {
             item{
@@ -166,39 +223,80 @@ fun GroupPage(navController: NavController){
                 )   {
                     Text(text = "Add(implement later,mby)")
                 }
-            }
-            /*items(ListOfPlayers){player ->
-                Card(
-                    //shape =
-                    shape = RoundedCornerShape(12.dp),
+                androidx.compose.material3.Text(
+                    text = "Staff",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(
                     modifier = Modifier
-                        .height(80.dp)
-                        .width(280.dp)
-                        //.alignment = Alignment.Center
-                        //.background(androidx.compose.ui.graphics.Color.LightGray)
-                        .padding(12.dp)
-                        .clickable {
-                            //ta mig vidare här
-                            navController.navigate("playerPage/${player.id}")
-                        }
-                ) {
-                    Text(
-                        text = player.name,
-                        style = MaterialTheme.typography.h4,
-                        textAlign = TextAlign.Center,
+                        .height(10.dp)
+                        .padding(10.dp)
+                )
+            }
+            items(group.listOfStaff) { staff ->
+                Column(modifier = Modifier.padding(vertical = 0.dp)
+                    .clickable { navController.navigate("TODO") }) {
+                    Surface(
+                        color = MaterialTheme.colorScheme.secondary,
+                        shape = RoundedCornerShape(150.dp),
                         modifier = Modifier
-                            //.padding(12.dp)
-                            .background(Color.LightGray)
-                    )
-                }*/
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                            .width(1100.dp)
+                    ) {
+                        Text(
+                            text = staff.name,
+                            style = MaterialTheme.typography.titleMedium,
+                            modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp),
+                            color = androidx.compose.ui.graphics.Color.White
+                        )
+                    }
+                }
+            }
+            item{
+                androidx.compose.material3.Text(
+                    text = "Players",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(
+                    modifier = Modifier
+                        .height(10.dp)
+                        .padding(10.dp)
+                )
+            }
+            items(group.players) { player ->
+                Column(modifier = Modifier.padding(vertical = 0.dp)
+                    .clickable { navController.navigate("PlayerPage/${group.id}/${player.id}") }) {
+                    Surface(
+                        color = MaterialTheme.colorScheme.secondary,
+                        shape = RoundedCornerShape(150.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                            .width(1100.dp)
+                    ) {
+                        Text(
+                            text = player.name,
+                            style = MaterialTheme.typography.titleMedium,
+                            modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp),
+                            color = androidx.compose.ui.graphics.Color.White
+                        )
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun PlayerPage(id: Int ,navController: NavController){
-    //val player = ListOfPlayers.find{it.id == id}?:return
+fun PlayerPage(navController: NavController, groupID: Int, playerID: Int){
+    //println("groups: " groupRepository.getGroupById(groupId).toString())
+    //println("groups: " groupRepository.getGroupById(groupId))
+    //val group = groupRepository.getGroupById(groupID).players.size
+    //val name = groupRepository.getGroupById(groupID).getPlayerById(playerID)
+    println(groupID)
+    println("kanske hit 3")
+    //println(name)
+    val player = groupRepository.getGroupById(groupID).getPlayerById(playerID)
+    println("kanske hit 4")
     Box( modifier = Modifier
         .fillMaxSize()
         .padding(bottom = 50.dp)
@@ -225,18 +323,18 @@ fun PlayerPage(id: Int ,navController: NavController){
                 .background(Color.LightGray)
         ) {
             Text(
-                text = "testing"
-                /*text = "Name: ${player.name} \n" +
+                //text = "testing"
+                text = "Name: ${player.name} \n" +
                         "Games Played: ${player.gamesPlayed} \n"+
                         "Goals: ${player.goals} \n"+
                         "Assists: ${player.assists} \n"+
                         "Yellow Cards: ${player.yellowCards} \n"+
                         "Red Cards: ${player.redCards}",
-                style = MaterialTheme.typography.h4,
+                style = typography.h4,
                 //textAlign = TextAlign.Center,
                 modifier = Modifier
                     .background(Color.LightGray)
-                    .padding(6.dp)*/
+                    .padding(6.dp)
 
             )
         }
