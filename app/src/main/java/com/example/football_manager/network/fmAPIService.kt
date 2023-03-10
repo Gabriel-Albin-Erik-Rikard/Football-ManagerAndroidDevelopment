39d160273bc3fb5263db2ee2ede2e9084e2a2966
Albin Forsberg <foal21fs@student.ju.se>
2023-03-10 16:43:43 +0100

    Added service functon

--- app/src/main/java/com/example/football_manager/network/fmAPIService.kt
+++ app/src/main/java/com/example/football_manager/network/fmAPIService.kt
@@ -1,10 +1,26 @@
 package com.example.football_manager.network
 
 import com.example.football_manager.model.News
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface fmAPIService {
     @GET("news.json")
     suspend fun getNews(): List<News>
+
+    companion object {
+        var apiService: fmAPIService? = null
+        fun getInstance(): fmAPIService {
+            if (apiService == null) {
+                apiService = Retrofit.Builder()
+                    .baseUrl("localhost:8080/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(apiService!!::class.java)
+            }
+            return apiService!!
+        }
+    }
 
 }
