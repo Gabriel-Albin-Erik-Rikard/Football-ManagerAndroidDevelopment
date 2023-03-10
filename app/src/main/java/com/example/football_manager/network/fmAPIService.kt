0d59a2b035023f73ec575664d23bf965ffd1872d
Albin Forsberg <foal21fs@student.ju.se>
2023-03-10 16:39:10 +0100

    Moved News model to model folder

--- app/src/main/java/com/example/football_manager/network/fmAPIService.kt
+++ app/src/main/java/com/example/football_manager/network/fmAPIService.kt
@@ -1,4 +1,10 @@
 package com.example.football_manager.network
 
+import com.example.football_manager.model.News
+import retrofit2.http.GET
+
 interface fmAPIService {
+    @GET("news.json")
+    suspend fun getNews(): List<News>
+
 }
