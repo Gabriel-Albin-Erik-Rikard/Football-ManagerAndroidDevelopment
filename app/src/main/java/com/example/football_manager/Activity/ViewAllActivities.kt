54253675227347ac178149c37e9c51eac5a0bb3a
Gabriel Adward <adga21bv@student.ju.se>
2023-03-13 15:48:35 +0100

    Fix som issues!

--- app/src/main/java/com/example/football_manager/Activity/ViewAllActivities.kt
+++ app/src/main/java/com/example/football_manager/Activity/ViewAllActivities.kt
@@ -2,32 +2,22 @@ package com.example.football_manager.Activity
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.football_manager.Activities
-import kotlin.math.ceil
-import kotlin.math.min
 
 @Composable
 fun ViewAllScreen(
