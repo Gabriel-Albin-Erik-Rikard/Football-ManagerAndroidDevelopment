08389e81ce9f3ea552ccd8a5a95dae19805a65d6
Gabriel Adward <adga21bv@student.ju.se>
2023-03-18 22:51:49 +0100

    Fixed Position of QR and added text above

--- app/src/main/java/com/example/football_manager/QR/QRScreen.kt
+++ app/src/main/java/com/example/football_manager/QR/QRScreen.kt
@@ -3,8 +3,9 @@ package com.example.football_manager
 
 import android.graphics.Bitmap
 import android.widget.ImageView
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import android.widget.Space
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,39 +15,67 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.qrcode.QRCodeWriter
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun QRScreen() {
-    AndroidView(factory = { ctx ->
-        ImageView(ctx).apply {
-            val size = 512
-            val hints = hashMapOf<EncodeHintType, Int>().also {
-                it[EncodeHintType.MARGIN] = 1
-            }
-            val url = "https://utbildning.sisuforlag.se/fotboll/tranare/tranarutbildning/"
-            val bits = QRCodeWriter().encode(
-                url,
-                BarcodeFormat.QR_CODE,
-                size,
-                size,
-                hints
-            )
-            val bitmap =
-                Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
-                    for (x in 0 until size) {
-                        for (y in 0 until size) {
-                            it.setPixel(
-                                x,
-                                y,
-                                if (bits[x, y]) Color.Black.toArgb() else Color.White.toArgb()
-                            )
-                        }
-                    }
-                }
-            setImageBitmap(bitmap)
-        }
-    }, modifier = Modifier
-        .width(width = 100.dp)
-        .height(height = 100.dp)
-    )
+    QRGenerator()
 }
+@Composable
+fun QRGenerator() {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .wrapContentHeight()
+    ) {
+        Text(
+            text = "Want To Become A Football-Coach?",
+            textAlign = TextAlign.Center,
+            style = TextStyle(fontSize = 20.sp),
+            modifier = Modifier
+                .align(Alignment.Center)
+
+        )
+        AndroidView(
+            factory = { ctx ->
+                ImageView(ctx).apply {
+                    val size = 256
+                    val hints = hashMapOf<EncodeHintType, Int>().also {
+                        it[EncodeHintType.MARGIN] = 1
+                    }
+                    val url = "https://utbildning.sisuforlag.se/fotboll/tranare/tranarutbildning/"
+                    val bits = QRCodeWriter().encode(
+                        url,
+                        BarcodeFormat.QR_CODE,
+                        size,
+                        size,
+                        hints
+                    )
+                    val bitmap =
+                        Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
+                            for (x in 0 until size) {
+                                for (y in 0 until size) {
+                                    it.setPixel(
+                                        x,
+                                        y,
+                                        if (bits[x, y]) Color.Black.toArgb() else Color.White.toArgb()
+                                    )
+                                }
+                            }
+                        }
+                    setImageBitmap(bitmap)
+                }
+            }, modifier = Modifier
+                .width(250.dp)
+                .height(1500.dp)
+               .padding(top = 300.dp)
+        )
+    }
+}
+
